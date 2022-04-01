934a3e06d3dac96d21fbee7875fafdff2e6c0d1b
MIkaela Braga <bragamikaela@gmail.com>
2022-04-01 15:41:10 -0300

    requisitos do 1 ao 3 rodando nos testes locais

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,26 +1,31 @@
 import * as express from 'express';
+import * as cors from 'cors';
 
 class App {
   public app: express.Express;
-  // ...
 
   constructor() {
-    // ...
+    this.app = express();
     this.config();
-    // ...
+    this.routes();
+    this.app.use(cors());
   }
 
   private config():void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT, PATCH');
       res.header('Access-Control-Allow-Headers', '*');
       next();
     };
-
     this.app.use(accessControl);
-    // ...
     this.app.use(express.json());
+  }
+
+  private routes() {
+    this.app.get('/', (req, res, _next) => {
+      res.status(200).json({ message: 'Ok!' });
+    });
   }
 
   // ...
